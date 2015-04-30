e7d443310d0ece933e3ff4eb457c052b77b346a9
Zornica <zornicaewgeniewa@gmail.com>
2015-04-30 15:31:17 +0300

    Add test about validator

--- src/service/ServiceValidatorImpl.java
+++ src/service/ServiceValidatorImpl.java
@@ -4,13 +4,11 @@ package service;
  * Created by Zornitsa Petkova on 4/30/15.
  */
 public class ServiceValidatorImpl implements ServiceValidator {
-  //private Service service;
-
   @Override
   public boolean validate(Service service) {
-    if (Integer.parseInt(service.age) >= 10 || Integer.parseInt(service.age) <= 100) {
-      return true;
+    if (Integer.parseInt(service.age) < 10 || Integer.parseInt(service.age) > 100) {
+      return false;
     }
-    return false;
+    return true;
   }
 }
---
+++ test/service/ServiceValidatorImplTest.java
@@ -0,0 +1,34 @@
+package service;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+/**
+ * Created by Zornitsa Petkova on 4/30/15.
+ */
+public class ServiceValidatorImplTest {
+  @Test
+  public void Validate() {
+    ServiceValidatorImpl service = new ServiceValidatorImpl();
+    Service age = new Service("12");
+    assertThat(service.validate(age), is(true));
+  }
+
+  @Test
+  public void ValidateToYoung() {
+    ServiceValidatorImpl service = new ServiceValidatorImpl();
+    Service age = new Service("9");
+    assertThat(service.validate(age), is(false));
+  }
+
+  @Test
+  public void ValidateTooOld() {
+    ServiceValidatorImpl service = new ServiceValidatorImpl();
+    Service age = new Service("102");
+    assertThat(service.validate(age), is(false));
+  }
+}
